f7eca2af6633132ea335ebf1724ac92be5751eb1
k1m0s <cucbku1@gmail.com>
2023-02-10 18:54:04 +0300

    add save(), clear(), get(), delete(),getAll() in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int size = 0;
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        if(size < storage.length) {
+            storage[size] = r;
+            size++;
+            System.out.println("Resume save in database.");
+        } else {
+            System.out.println("Resume database is full.");
+        }
     }
 
     Resume get(String uuid) {
+        for(int i = 0; i < size; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for(int i = 0; i < size; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                for(int j = i; j < size - 1; j++) {
+                    storage[i] = storage[j + 1];
+                }
+                storage[size - 1] = null;
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
