3ff1bb618648ed24032091d28445a3b74183ace1
k1m0s <cucbku1@gmail.com>
2023-02-21 13:30:22 +0300

    Fix equals

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -55,7 +55,7 @@ public class ArrayStorage extends AbstractArrayStorage {
 
     protected int getIndex(String uuid){
         for(int i = 0; i < size; i++) {
-            if(storage[i].getUuid().equals(uuid)) {
+            if(uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
